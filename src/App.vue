de9e23150898307d768e1d8a3161613b1de9c541
jskjott <jonathan@jskjott.com>
2019-09-21 10:43:09 -0400

    add menu

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
 		<div>
 			<h1>Fork Visualiser</h1>
 		</div>
-		<Menu/>
-		<Visual/>
+		<Menu @repoLink="repoUpdate" />
+		<Visual v-bind="{ currentRepo }" />
 	</div>
 </template>
 
@@ -19,6 +19,9 @@ import apiKey from '../githubAPIKey'
 
 const TOKEN = apiKey
 
+let currentRepo: string
+let graphQL: object
+
 const vue = Vue.extend({
 	name: 'app',
 	components: {
@@ -27,22 +30,30 @@ const vue = Vue.extend({
 	},
 	data() {
 		return {
-			graphQL: null,
+			currentRepo,
+			graphQL,
 		}
 	},
-	mounted: function() {
-		fetch('https://api.github.com/graphql', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'Authorization': `token ${TOKEN}`,
-			},
-			body: JSON.stringify({ query: graphQLQuery }),
-		})
-			.then(res => res.json())
-			.then(res => {
-				this.graphQL = res.data
+	mounted: function() {},
+	methods: {
+		repoUpdate: function(repo: string) {
+			this.currentRepo = repo
+			this.fetchData(repo)
+		},
+		fetchData: async function(repo: string) {
+			this.graphQL = await fetch('https://api.github.com/graphql', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'Authorization': `token ${TOKEN}`,
+				},
+				body: JSON.stringify({ query: graphQLQuery }),
 			})
+				.then(res => res.json())
+				.then(res => {
+					return res.data
+				})
+		},
 	},
 	watch: {
 		graphQL: function() {
@@ -55,5 +66,13 @@ export default vue
 </script>
 
 <style>
+body {
+	margin: 0;
+	font-family: Arial, Helvetica, sans-serif;
+	color: #c5cbe1;
+}
 
+h1 {
+	margin-left: 2rem;
+}
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,16 +1,58 @@
 <template>
-	<div class="menu"></div>
+	<div class="menu">
+		<input
+			v-model="repository"
+			placeholder="Paste in link to a Github repository"
+		/>
+		<button v-on:click="repoLink(repository)">
+			<h2>Visualise Forks</h2>
+		</button>
+	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+let repository: string
+
 export default Vue.extend({
 	name: 'Menu',
+	data() {
+		return {
+			repository,
+		}
+	},
 	props: {
-		msg: String,
+		currentRepo: String,
+	},
+	methods: {
+		repoLink(repo: string) {
+			this.$emit('repoLink', { repo })
+		},
 	},
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.menu {
+	display: grid;
+	justify-items: center;
+}
+
+input {
+	background-color: #d0d2df;
+	color: #626369;
+	border: none;
+	height: 2rem;
+	font-size: 1rem;
+	width: 100%;
+	margin-bottom: 1rem;
+	text-align: center;
+}
+
+button {
+	background-color: #d0d2df;
+	color: #626369;
+	padding: 0 0.75rem 0 0.75rem;
+}
+</style>
